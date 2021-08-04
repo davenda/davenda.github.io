006c36e73811e20217c6e0df540d2a31c9144915
davend <dawitend2@gmail.com>
2021-08-04 16:38:10 -0500

    Added maze assignment

---
+++ W3D2_Maze/maze.js
@@ -0,0 +1,33 @@
+$(function(){
+    var started;
+    $('.boundary').on('mouseover', failed);
+    $('#end').on('mouseover', function(evt){
+        if(started){
+            $('#status').text("You Win! :]");
+        }
+    });
+    $('#end').on('mouseleave', function(evt){
+        if(started){
+            reset();
+        }
+    });
+    $('#start').on('click', function(evt){
+        if(!started){
+            started = true;
+        }else{
+            reset();
+        }
+    });
+    $('#maze').on('mouseleave', failed);
+    function failed(evt){
+        if(started){
+            $('#status').text("Sorry, You Lost. :[");
+            $('.boundary').not('.example').addClass("youlose");
+        }
+    } 
+    function reset(){
+        $('.boundary').removeClass("youlose");
+        started = null;
+        $('#status').text("Click the \"S\" to begin");
+    }
+})
