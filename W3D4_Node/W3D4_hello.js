ffa296341c62251ce96ee6aecf437af745ce2a24
davend <dawitend2@gmail.com>
2021-08-05 16:10:26 -0500

    modified the first file

--- W3D4_Node/W3D4_hello.js
+++ W3D4_Node/W3D4_hello.js
@@ -3,26 +3,12 @@ const readline = require('readline').createInterface({
     output: process.stdout,
 });
 
-// readline.question("What is your name? ", name => {
-//     console.log(`Welcome ${name}`);
-//     readline.question('What is your age? ', age => {
-//         if(age < 16)
-//             console.log("You're not allowed to drive in Iowa");
-//         else
-//             console.log("You're allowed to get a drivers license in Iowa");
-//     });
-// });
-let count = 0;
-const getNum = function getNumber(name){
-    if(name == 'stop'){
-        console.log(count);
-        readline.close();
-    }
-    else{
-        count++;
-        readline.question("Enter a number: ", name => {
-            getNumber(name);
-        })
-    }
-}
-getNum();
+readline.question("What is your name? ", name => {
+    console.log(`Welcome ${name}`);
+    readline.question('What is your age? ', age => {
+        if(age < 16)
+            console.log("You're not allowed to drive in Iowa");
+        else
+            console.log("You're allowed to get a drivers license in Iowa");
+    });
+});
